8bffa27204733e5485d0299a99ca534b447967e9
Ben Zhang <ben@theclarityteam.net>
2024-06-11 11:37:09 -0700

    made reunion timer

---
+++ scripts/datescript.js
@@ -0,0 +1,31 @@
+// countdown.js
+
+// Set the date we're counting down to
+const countDownDate = new Date("Jul 10, 2024 10:15:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(() => {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+module.exports = x;
