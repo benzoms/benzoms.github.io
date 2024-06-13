151ca6afda8a161e7ba42e7592d23f139cfb0d2a
Ben Zhang <ben@theclarityteam.net>
2024-06-12 20:38:02 -0700

    added navigation, potd

--- scripts/datescript.js
+++ scripts/datescript.js
@@ -62,6 +62,46 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const library = document.querySelector('#library');
+    
+    library.addEventListener('click', () => {
+        window.location.replace("file:///Users/benzhang/Mine/Tech/Projects/backgrounds/benzoms.github.io/pages/date/library.html");
+        // if (map.classList.contains('show')) {
+        //     map.classList.remove('show');
+        // } else {
+        //     map.classList.add('show');
+        // }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const egggarden = document.querySelector('#egggarden');
+    
+    egggarden.addEventListener('click', () => {
+        window.location.replace("file:///Users/benzhang/Mine/Tech/Projects/backgrounds/benzoms.github.io/pages/date.html");
+        // if (map.classList.contains('show')) {
+        //     map.classList.remove('show');
+        // } else {
+        //     map.classList.add('show');
+        // }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const photoforest = document.querySelector('#photoforest');
+    
+    photoforest.addEventListener('click', () => {
+        window.location.replace("file:///Users/benzhang/Mine/Tech/Projects/backgrounds/benzoms.github.io/pages/date/photoforest.html");
+        // if (map.classList.contains('show')) {
+        //     map.classList.remove('show');
+        // } else {
+        //     map.classList.add('show');
+        // }
+    });
+});
+
 // document.addEventListener('DOMContentLoaded', () => {
 //     const div = document.querySelector('#mapicondiv');
 //     const icon = document.querySelector('#mapicon');
